d6bf92f70347ec0e076d4b16d11766fd0bf9078c
Adam Hlaváček <git@adamhlavacek.com>
2023-09-23 18:24:37 +0200

    feat: render answers

--- compile.py
+++ compile.py
@@ -1,4 +1,6 @@
 import asyncio
+import math
+from typing import Iterator
 from yaml import load
 from sys import argv
 from pathlib import Path
@@ -7,35 +9,56 @@ from subprocess import check_call
 from tempfile import mkdtemp, mkstemp
 from shutil import rmtree, move
 from os import close
+from string import ascii_uppercase
+from itertools import product
+from multiprocessing import cpu_count
 
 
-def format_question(question: dict, index: int, questions_total: int) -> str:
+def format_question(question: dict, index: int, questions_total: int, answers: bool, rnd: Random) -> str:
     output = f"### ({index + 1}/{questions_total}) {question['question']}\n\n"
 
     if 'text' in question:
-        for text in question['text']:
-            output += f"{text}: " + '\\_' * (76 - len(text)) + "\n\n"
+        for text, answer in question['text'].items():
+            output += f"{text} "
+            if answers:
+                output += f"**_{answer}_**"
+            else:
+                output +='\\_' * (76 - len(text))
+            output += "\n\n"
     if 'options' in question:
-        output += '(' + (')' + (' ' * 10) +  '(').join(question['options']) + ')\n'
+        options = []
+        for option in question['options']:
+            correct_answer = False
+            if option.endswith('<'):
+                correct_answer = True
+                option = option[:-1]
+            if answers and correct_answer:
+                option = "**_{" + option + "}_**"
+            else:
+                option = "(" + option + ")"
+            options.append(option)
+
+        rnd.shuffle(options)
+        output += (' ' * 10).join(options) + "\n\n"
 
     return output
 
 
-def format_category(category_name: str, category_data: dict, index: int, questions_total: int, rnd: Random) -> str:
+def format_category(category_name: str, category_data: dict, index: int, questions_total: int, rnd: Random, answers: bool) -> str:
     output = ""
     for question in rnd.sample(category_data['questions'], k=category_data['select']):
-        output += format_question(question, index, questions_total) + '\n'
+        output += format_question(question, index, questions_total, answers, rnd) + '\n'
         index += 1
     return output
 
 
-def format_test(test_input: dict, version: int) -> str:
+def format_test(test_input: dict, test_group: str, answers: bool) -> str:
     test_input = dict(test_input)
 
-    version_str = hex(10 + version).upper()[2:]
+    group_number = int.from_bytes(test_group.encode('utf8'), 'little')
 
     output = f"# Test: {test_input['$name']}\n\n" \
-             f"*Skupina: {version_str}*\n\n" \
+             f"*Skupina: {test_group}*" + (", autorské řešení" if answers else "") + "\n\n" \
              f"**Jméno**: " + '\\_' * 40 + ' ' + \
              f"**Datum**: " + '\\_' * 20 + '\n\n' \
              '## Zadání\n\n'
@@ -43,27 +66,27 @@ def format_test(test_input: dict, version: int) -> str:
 
     categories = test_input
 
-    rnd = Random(version)
+    rnd = Random(group_number)
     question_index = 0
     questions_total = sum(map(lambda x: x['select'], categories.values()))
 
     for category_name, category_data in categories.items():
-        output += format_category(category_name, category_data, question_index, questions_total, rnd) + "\n"
+        output += format_category(category_name, category_data, question_index, questions_total, rnd, answers) + "\n"
         question_index += category_data['select']
     return output
 
 
-async def create_test_pdf(test_input: dict, index: int, dir_pdf: Path) -> Path:
+async def create_test_pdf(test_input: dict, group: str, dir_pdf: Path, answers: bool) -> Path:
     handle, file_md_path = mkstemp(prefix='assigment_', suffix='.md')
     close(handle)
 
     file_md = Path(file_md_path)
-    file_pdf = dir_pdf.joinpath('%03d.pdf' % index)
+    file_pdf = dir_pdf.joinpath(f'group_{group}.pdf')
 
     file_pdf.parent.mkdir(exist_ok=True, parents=True)
 
     with file_md.open('w') as f:
-        f.write(format_test(test_input, index))
+        f.write(format_test(test_input, group, answers))
     process = await asyncio.subprocess.create_subprocess_exec(
         "pandoc",
         "--pdf-engine=xelatex",
@@ -78,18 +101,38 @@ async def create_test_pdf(test_input: dict, index: int, dir_pdf: Path) -> Path:
     return file_pdf
 
 
+def generate_group(iterations: int = math.inf) -> Iterator[str]:
+    length = 1
+    while iterations > 0:
+        for combo in product(ascii_uppercase, repeat=length):
+            yield ''.join(combo)
+            iterations -= 1
+            if iterations <= 0:
+                break
+        length += 1
+
+
+async def gather_with_concurrency(n, *coros):
+    semaphore = asyncio.Semaphore(n)
+
+    async def sem_coro(coro):
+        async with semaphore:
+            return await coro
+    return await asyncio.gather(*(sem_coro(c) for c in coros))
+
+
 async def main() -> None:
     test_input_file = Path(argv[1])
     test_count = int(argv[2])
 
     dir_pdf = Path(mkdtemp(prefix="testy_pdf_", dir='/ram'))
-    file_out = Path('/ram/testy.pdf')
+    file_out = Path('testy.pdf')
 
     with test_input_file.open('r') as f:
         test_input = load(f)
 
-    pdfs_tasks = [asyncio.create_task(create_test_pdf(test_input, i, dir_pdf)) for i in range(test_count)]
-    file_pdfs = await asyncio.gather(*pdfs_tasks)
+    pdfs_tasks = [create_test_pdf(test_input, group, dir_pdf, True) for group in generate_group(test_count)]
+    file_pdfs = await gather_with_concurrency(max(cpu_count() - 1, 1), *pdfs_tasks)
 
     check_call(["pdftk"] + [f"{x.name}" for x in file_pdfs] + ["cat", "output", "merged.pdf"], cwd=dir_pdf)
     move(dir_pdf.joinpath("merged.pdf"), file_out)
